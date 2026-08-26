c16fc28041ab10974f7cb38a9c7fee3061e612a7
Joshua Luke Parris <130725599+joshualparris@users.noreply.github.com>
2026-08-26 15:01:17 +1000

    feat: include full personal curriculum in module library

--- src/data/allModules.ts
+++ src/data/allModules.ts
@@ -1,5 +1,6 @@
 import { modules as dcsModules } from './modules';
 import { outdoorEducationModules } from './outdoorEducationModules';
+import { personalCurriculumModules } from './personalCurriculum';
 
-export const allModules = [...dcsModules, ...outdoorEducationModules];
+export const allModules = [...dcsModules, ...outdoorEducationModules, ...personalCurriculumModules];
 export default allModules;
