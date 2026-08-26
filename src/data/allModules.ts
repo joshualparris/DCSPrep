7b895cf1ccc5e53bfde75165ec193a49f12aa0e0
Joshua Luke Parris <130725599+joshualparris@users.noreply.github.com>
2026-08-26 14:38:25 +1000

    feat: combine DCS and Outdoor Education modules

---
+++ src/data/allModules.ts
@@ -0,0 +1,5 @@
+import { modules as dcsModules } from './modules';
+import { outdoorEducationModules } from './outdoorEducationModules';
+
+export const allModules = [...dcsModules, ...outdoorEducationModules];
+export default allModules;
