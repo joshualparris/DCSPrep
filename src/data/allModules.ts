60207ed63bbc787ce5442feeb446bde25d7eee03
Joshua Luke Parris <130725599+joshualparris@users.noreply.github.com>
2026-08-26 15:28:05 +1000

    feat: register AB401A Genesis module

--- src/data/allModules.ts
+++ src/data/allModules.ts
@@ -1,5 +1,6 @@
 import { modules as dcsModules } from './modules';
 import { outdoorEducationModules } from './outdoorEducationModules';
+import { genesisModule } from './genesisModule';
 
-export const allModules = [...dcsModules, ...outdoorEducationModules];
+export const allModules = [...dcsModules, ...outdoorEducationModules, genesisModule];
 export default allModules;
