204bfcb7e909cb67ca2bb0f467f80a3155e4fc97
Joshua Luke Parris <130725599+joshualparris@users.noreply.github.com>
2026-08-26 15:06:45 +1000

    revert: restore pre-curriculum module list

--- src/data/allModules.ts
+++ src/data/allModules.ts
@@ -1,6 +1,5 @@
 import { modules as dcsModules } from './modules';
 import { outdoorEducationModules } from './outdoorEducationModules';
-import { personalCurriculumModules } from './personalCurriculum';
 
-export const allModules = [...dcsModules, ...outdoorEducationModules, ...personalCurriculumModules];
+export const allModules = [...dcsModules, ...outdoorEducationModules];
 export default allModules;
